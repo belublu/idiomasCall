c3650ea3330b29c78c5c67732466ae39efcb85dd
Belen Torres <belentorress@gmail.com>
2023-05-30 22:22:45 -0300

    Creación de proyecto, carga de recursos, desarrollo de index, estilos y js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("#show-login").addEventListener("click", function() {
+    document.querySelector(".popUp").classList.add("active");
+    document.querySelector(".overlay").style.display = "block";
+    document.body.classList.add("blur"); 
+});
+
+document.querySelector(".popUp .close-btn").addEventListener("click", function() {
+    document.querySelector(".popUp").classList.remove("active");
+    document.querySelector(".overlay").style.display = "none";
+    document.body.classList.remove("blur");
+});

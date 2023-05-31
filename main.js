1bebeb9da8d173fd88a04f25dd7ec958915ff22c
Belen Torres <belentorress@gmail.com>
2023-05-30 22:45:18 -0300

    Corrección en js y en el popup del boton login que afectaba al nav

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 document.querySelector("#show-login").addEventListener("click", function() {
     document.querySelector(".popUp").classList.add("active");
     document.querySelector(".overlay").style.display = "block";
-    document.body.classList.add("blur"); 
-});
+/*     document.body.classList.add("blur"); 
+ */});
 
 document.querySelector(".popUp .close-btn").addEventListener("click", function() {
     document.querySelector(".popUp").classList.remove("active");
     document.querySelector(".overlay").style.display = "none";
-    document.body.classList.remove("blur");
+    /* document.body.classList.remove("blur"); */
 });
+
